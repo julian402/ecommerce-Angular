4fa9b49079db0afc860aca9192e85d4c2fc06c19
Julian402 <corredor.jul@hotmail.com>
2024-10-16 19:56:30 -0500

    DetailPage

--- ecommerceProject/src/app/app.routes.ts
+++ ecommerceProject/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'detail/:id', component: DetailComponent },
+];
